f92eb7420a8219d7b494a022de9c87085755d015
YGXB-net <ygxb-net@outlook.com>
2022-12-18 23:17:09 +0800

    更改部分代码格式

--- test/update/upload_test.go
+++ test/update/upload_test.go
@@ -10,7 +10,6 @@ import (
 )
 
 func Test_Upload(t *testing.T) {
-
 	var bufReader bytes.Buffer
 	mpWriter := multipart.NewWriter(&bufReader)
 	fw, err := mpWriter.CreateFormFile("file", "a.txt")
@@ -19,11 +18,11 @@ func Test_Upload(t *testing.T) {
 		return
 	}
 
-	//f, err2 := os.Open("../../config.yml")
-	//if err2 != nil {
+	// f, err2 := os.Open("../../config.yml")
+	// if err2 != nil {
 	//	t.Fatal(err2)
-	//}
-	//_, err = io.Copy(fw, f)
+	// }
+	// _, err = io.Copy(fw, f)
 	f, err2 := os.ReadFile("../../config.yml")
 	if err2 != nil {
 		t.Fatal(err2)
@@ -39,7 +38,6 @@ func Test_Upload(t *testing.T) {
 }
 
 func Test_Upload_Local_Image(t *testing.T) {
-
 	var fileName = "test.png"
 	var filePath = `C:\Users\LXM\Pictures\4456352-8938615d16aa9d71.png`
 
@@ -53,7 +51,6 @@ func Test_Upload_Local_Image(t *testing.T) {
 }
 
 func Test_Upload_Network_Image(t *testing.T) {
-
 	var fileName = "test.webp"
 	var fileURL = `https://ts1.cn.mm.bing.net/th/id/R-C.c2f6165e1acf6b986a20e8b676f13bdd?rik=Dm%2bD%2fgeM4oqgBw&riu=http%3a%2f%2fwww.quazero.com%2fuploads%2fallimg%2f140303%2f1-140303215045.jpg&ehk=sdIsCQj%2bvjKfUs%2fDw%2fZekILroLb1ALwbKghApSPIq4U%3d&risl=&pid=ImgRaw&r=0`
 	data := httpapi.GetNetworkImageData(fileURL)
